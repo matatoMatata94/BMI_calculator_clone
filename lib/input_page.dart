220b806ff9c83c226f3a6609e27bf9915b15c303
matatoMatata94 <73385202+matatoMatata94@users.noreply.github.com>
2021-06-04 21:50:41 +0200

    Added new page

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,7 +1,7 @@
+import 'package:bmi_calculator/cirle_icon_button.dart';
 import 'package:bmi_calculator/constants.dart';
 import 'package:bmi_calculator/gender.dart';
 import 'package:bmi_calculator/reusable_card.dart';
-import 'package:bmi_calculator/round_cirle_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
